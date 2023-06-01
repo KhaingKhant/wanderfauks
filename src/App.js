b58d515fa4a3112fd0641446419984a4da4dfc96
Khaing Khant <khaingkhantjob@gmail.com>
2023-06-01 17:14:58 +0630

    remove unnecessary codes and clean

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 import Header from './pages/Header';
 import Work from './pages/Work';
-import Home from './pages/Home';
 
 const App = () => {
   return (
     <div>
         <Header />
-        {/* <Home /> */}
         <Work/>
     </div>
   );
